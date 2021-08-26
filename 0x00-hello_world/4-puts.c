a137e3c65624f20359cc57764763067d3c2606c3
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-26 13:54:41 +0000

    My First standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * We will be printing a sentence with this betty style
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
