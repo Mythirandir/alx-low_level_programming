0c7878bb7cb7032d48262227d1a960012197a757
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-26 14:29:05 +0000

    My Second standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - We are still printing out a sentence, just with a diff function
+ * Description: We are using the "printf" function here
+ * Return - wil be 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
