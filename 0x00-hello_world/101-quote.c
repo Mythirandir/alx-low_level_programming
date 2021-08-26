67b09fabb3a6164b87bccc404e292e7db36bab1f
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-26 16:38:40 +0000

    I am still a bit confused about this though

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote
+ * Return: 1 is success
+ */
+
 int main(void)
 {
-	const char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
