f6fb3f935668c96e0706abcbfa8346bd133074aa
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-26 14:20:58 +0000

    My First standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints string
- * Description: Prints "\"Programming is like building a multilingual puzzle"
- * Return: 0    
+ * main - Prints the string
+ * Description: We are printing with the "puts" function
+ * Return: value returned 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
