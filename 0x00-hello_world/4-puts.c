79a6b1aded61abd3a41c349006a6039d4cb1a310
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-26 14:13:30 +0000

    My First standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0    
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
