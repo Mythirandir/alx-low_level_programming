505aca3a6312196f20d61276b6d5d3398bac6845
Mythirandir <abiolaakinwoleola@gmail.com>
2021-08-27 19:58:44 +0000

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out all numbers under 10
+ * Description: It has been described
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	printf("\n");
+	return (0);
+}
