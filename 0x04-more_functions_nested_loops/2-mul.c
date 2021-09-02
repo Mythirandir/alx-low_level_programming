476dd8b38e2cef7fc76be72a9e91108e6ed9d07e
Mythirandir <abiolaakinwoleola@gmail.com>
2021-09-02 21:12:41 +0000

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,13 @@
 /**
  * main - check the code
  * mul: We are using this identifier to multiply 2 numbers
+ * a: first integer for multiplication
+ * b: second integer for multiplication
  * Return: Always 0.
  */
 int mul(int a, int b)
 {
 	int result;
 	result = a * b;
-	return result;
+	return (result);
 }
