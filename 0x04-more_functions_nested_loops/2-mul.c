3d84fedcc1a32d80893daf83fcca0571e9457fcf
Mythirandir <abiolaakinwoleola@gmail.com>
2021-09-02 21:07:47 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * mul: We are using this identifier to multiply 2 numbers
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	result = a * b;
+	return resul;t
+}
