79addba42a707e98fb178cc46c23250777570772
Mythirandir <abiolaakinwoleola@gmail.com>
2021-09-02 19:32:13 +0000

    Update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && year % 100) || (!(year % 100) && !(year % 4000)))
 	{
-		if (month >2)
+		if (month > 2)
 		{
 			day++;
 		}
