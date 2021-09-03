91e4ed27ea40f7a580de77a4d5698d3291cd7b53
Mythirandir <abiolaakinwoleola@gmail.com>
2021-09-03 18:31:40 +0000

    Update 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,17 +4,15 @@
  */
 void more_numbers(void)
 {
-	int i;
-
-	char c;
+	int i, c;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c > 9)
+			if (c >= 9)
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n')
 	}
