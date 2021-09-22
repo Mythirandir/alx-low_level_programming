ca7ed37fbf942064229043d54269648b635d8fc0
Mythirandir <abiolaakinwoleola@gmail.com>
2021-09-22 20:53:54 +0000

    All commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of the program
+ * @argc: The size of the argv array
+ * @argv: An array of size argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
